e073681387c69f561a46a58828927eb1fde72aca
Jun Yu <johnyu617@yahoo.com>
2019-01-25 22:03:33 -0800

    remove resolver

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,4 +1,3 @@
-@GrabResolver(name='downloadGrapes', root='http://localhost:8081/nexus/content/repositories/jun-test-repo-id/')
 @Grab(group='sie.kamaji', module='jenkins-test', version='1.0.2')
 import SomeLibrary
 def call(){
