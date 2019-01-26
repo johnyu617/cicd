ecd088f1bf95f4746079281ed4d3f83258d779e8
Jun Yu <johnyu617@yahoo.com>
2019-01-26 12:02:49 -0800

    Update foo.groovy

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,6 +1,6 @@
 //@Grab('org.apache.commons:commons-math3:3.4.1')
 @GrabResolver(name='custom', root='http://localhost:8081/nexus/content/repositories/ivy/', m2Compatible='true')
-@Grab('sie.kamaji:jenkins-test:1.1.0')
+@Grab('sie:jenkins-test:1.1.1')
 import SomeLibrary
 def parallelize(String s) {
   return SomeLibrary.getStatus(s)
