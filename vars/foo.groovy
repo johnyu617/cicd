80c90dcbf79872f1eed5647bbc9c5c71d4163e46
Jun Yu <johnyu617@yahoo.com>
2019-01-26 11:44:43 -0800

    Update foo.groovy

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,7 +1,7 @@
-@Grab('org.apache.commons:commons-math3:3.4.1')
-import org.apache.commons.math3.primes.Primes
-void parallelize(int count) {
-  if (!Primes.isPrime(count)) {
-    error "${count} was not prime"
-  }
+//@Grab('org.apache.commons:commons-math3:3.4.1')
+@GrabResolver(name='custom', root='http://localhost:8081/nexus/content/repositories/ivy/', m2Compatible='true')
+@Grab('sie.kamaji:jenkins-test:1.1.0')
+import SomeLibrary
+def parallelize(String s) {
+  return SomeLibrary.getStatus(s)
 }
