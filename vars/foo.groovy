0cb935589b025dec03387cbde77c64572be515fd
Jun Yu <johnyu617@yahoo.com>
2019-01-25 21:52:53 -0800

    Update foo.groovy

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,4 +1,4 @@
-@GrabResolver(name='restlet', root='http://localhost:8081/nexus/content/repositories/jun-test-repo-id/')
+@GrabResolver(name='jun-nexus', root='http://localhost:8081/nexus/content/repositories/jun-test-repo-id/')
 @Grab(group='sie.kamaji', module='jenkins-test', version='1.0.2')
 import SomeLibrary
 def call(){
