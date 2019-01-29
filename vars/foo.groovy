8184305a41cdd37e973ad22fa9a32503f1b22496
Jun Yu <johnyu617@yahoo.com>
2019-01-29 14:30:28 -0800

    Update foo.groovy

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,7 +1,7 @@
 //@Grab('org.apache.commons:commons-math3:3.4.1')
-@GrabResolver(name='custom', root='http://192.168.1.194:8081/nexus/content/repositories/jun-test-repo-id/', m2Compatible='true')
-@Grab('sie:jenkins-test:1.1.1')
+//@GrabResolver(name='custom', root='http://192.168.1.194:8081/nexus/content/repositories/jun-test-repo-id/', m2Compatible='true')
+@Grab('com.amazonaws:aws-java-sdk:1.11.125')
 import SomeLibrary
 def parallelize(String s) {
-  return SomeLibrary.getStatus(s)
+  //return SomeLibrary.getStatus(s)
 }
