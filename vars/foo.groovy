576927a4c9bc8c292f8593025636dfe328be38a0
Jun Yu <johnyu617@yahoo.com>
2019-01-29 14:56:33 -0800

    to 327

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,6 +1,6 @@
 //@Grab('org.apache.commons:commons-math3:3.4.1')
 //@GrabResolver(name='custom', root='http://192.168.1.194:8081/nexus/content/repositories/jun-test-repo-id/', m2Compatible='true')
-//@Grab('com.amazonaws:aws-java-sdk:1.11.327')
+@Grab('com.amazonaws:aws-java-sdk:1.11.327')
 //import SomeLibrary
 def parallelize(String s) {
   //return SomeLibrary.getStatus(s)
